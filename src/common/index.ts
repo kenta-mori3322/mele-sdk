69323d2c2ae7b5784864b47f5dba08b70d78734d
Vidor Gencel <vidorge@gmail.com>
2020-06-17 12:32:56 +0200

    common types

---
+++ src/common/index.ts
@@ -0,0 +1,28 @@
+export interface SDKCoin {
+    denom: string
+    amount: number
+}
+
+export interface PubKey {
+    type: string
+    value: string
+}
+
+export interface AccSignInfo {
+    address: string
+    accountNumber: number
+    sequence: number
+}
+
+export interface Account {
+    type: string
+    value: AccountDetail
+}
+
+export interface AccountDetail {
+    address: string
+    coins: SDKCoin[]
+    public_key: PubKey
+    account_number: number
+    sequence: number
+}
