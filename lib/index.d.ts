c76be62e73f8b50c25c8432866e3f2222828b464
Andrej Budinčević <andrew@hotmail.rs>
2020-06-18 11:17:27 +0200

    Build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,5 @@
 import * as Utils from './utils';
-import { KeyPairSigner } from './signer';
+import { KeyPairSigner, MnemonicSigner, DefaultSigner } from './signer';
 import { Mele } from './mele';
 export * from './query';
-export { Mele, Utils, KeyPairSigner };
+export { Mele, Utils, KeyPairSigner, MnemonicSigner, DefaultSigner };
--- lib/signer/index.d.ts
+++ lib/signer/index.d.ts
@@ -1,4 +1,5 @@
 import { Signer } from './signer';
 import { DefaultSigner } from './default';
 import { KeyPairSigner } from './keyPair';
-export { Signer, DefaultSigner, KeyPairSigner };
+import { MnemonicSigner } from './mnemonic';
+export { Signer, DefaultSigner, KeyPairSigner, MnemonicSigner };
