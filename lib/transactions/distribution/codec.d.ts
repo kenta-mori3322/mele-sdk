53a96cb69cd5ef0220521334e4e43e4e11839c63
Andrej Budinčević <andrew@hotmail.rs>
2020-09-07 21:34:51 +0200

    Build

--- lib/transactions/distribution/codec.d.ts
+++ lib/transactions/distribution/codec.d.ts
@@ -2,4 +2,5 @@ export declare const Codec: {
     'cosmos-sdk/MsgWithdrawDelegationReward': any;
     'cosmos-sdk/MsgWithdrawValidatorCommission': any;
     'cosmos-sdk/MsgModifyWithdrawAddress': any;
+    'cosmos-sdk/MsgFundCommunityPool': any;
 };
--- lib/transactions/distribution/index.d.ts
+++ lib/transactions/distribution/index.d.ts
@@ -1,11 +1,14 @@
 import { Transaction, TransactionApi } from '../index';
+import * as Types from '../../common';
 export declare const Msgs: {
     makeWithdrawDelegationRewardMsg(delegator: string, validator: string): any[];
     makeWithdrawValidatorCommissionMsg(validator: string): any[];
     makeModifyWithdrawAddressMsg(delegator: string, withdrawAddress: string): any[];
+    makeFundCommunityPool(amount: Types.SDKCoin[], depositor: string): any[];
 };
 export default class Distribution extends TransactionApi {
     withdrawDelegationReward(validator: string): Transaction;
     withdrawValidatorCommission(validator: string): Transaction;
     modifyWithdrawAddress(withdrawAddress: string): Transaction;
+    fundCommunityPool(amount: Types.SDKCoin[]): Transaction;
 }
