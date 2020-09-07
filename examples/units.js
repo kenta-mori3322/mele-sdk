b759a733f34b5dc6f6024010585c15031cb44576
Andrej Budinčević <andrew@hotmail.rs>
2020-09-07 10:40:13 +0200

    Add unit conversion examples

---
+++ examples/units.js
@@ -0,0 +1,7 @@
+const { Utils } = require('../lib/mele-sdk.cjs.js')
+
+console.log(Utils.fromUmelg('1000000000', 'melg'))
+console.log(Utils.toUmelg('10', 'melg'))
+
+console.log(Utils.fromUmelc('100000000000', 'melc'))
+console.log(Utils.toUmelc('100', 'melc'))
