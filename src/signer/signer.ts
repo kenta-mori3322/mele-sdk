1c921ddd4ed7f1b7cf5252a5f40f6425570f6690
Andrej Budinčević <andrew@hotmail.rs>
2021-04-07 11:01:46 +0200

    Change signer interface to support new signers

--- src/signer/signer.ts
+++ src/signer/signer.ts
@@ -1,13 +1,11 @@
+import { AccountData, DirectSignResponse } from '@cosmjs/proto-signing'
+import { SignDoc } from '../transport/codec/cosmos/tx/v1beta1/tx'
+
 export interface Signer {
     getAddress(): string
     getPrivateKey(): string
     getPublicKey(): string
-    signTransaction(
-        msgs: any[],
-        chainId: string,
-        fee: number,
-        sequence: number,
-        accountNumber: number
-    ): string
+    signDirect(address: string, signDoc: SignDoc): Promise<DirectSignResponse>
     signMessage(msg: string): string
+    getAccounts(): AccountData[]
 }
