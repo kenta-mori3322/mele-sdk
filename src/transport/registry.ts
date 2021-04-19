6e169050b4692f1fdfe4723aae1bbb124bdb301a
Andrej Budinčević <andrew@hotmail.rs>
2021-04-19 21:24:14 +0200

    Register new codecs

--- src/transport/registry.ts
+++ src/transport/registry.ts
@@ -1,9 +1,31 @@
 import { GeneratedType, Registry } from '@cosmjs/proto-signing'
 
 import { MsgMultiSend } from './codec/cosmos/bank/v1beta1/tx'
+import {
+    MsgFundCommunityPool,
+    MsgSetWithdrawAddress,
+    MsgWithdrawDelegatorReward,
+    MsgWithdrawValidatorCommission,
+  } from "./codec/cosmos/distribution/v1beta1/tx";
+  import {
+    MsgBeginRedelegate,
+    MsgCreateValidator,
+    MsgDelegate,
+    MsgEditValidator,
+    MsgUndelegate,
+  } from "./codec/cosmos/staking/v1beta1/tx";
 
 export const defaultRegistryTypes: ReadonlyArray<[string, GeneratedType]> = [
     ['/cosmos.bank.v1beta1.MsgMultiSend', MsgMultiSend],
+    ["/mele.distribution.v1beta1.MsgFundCommunityPool", MsgFundCommunityPool],
+    ["/mele.distribution.v1beta1.MsgSetWithdrawAddress", MsgSetWithdrawAddress],
+    ["/mele.distribution.v1beta1.MsgWithdrawDelegatorReward", MsgWithdrawDelegatorReward],
+    ["/mele.distribution.v1beta1.MsgWithdrawValidatorCommission", MsgWithdrawValidatorCommission],
+    ["/mele.staking.v1beta1.MsgBeginRedelegate", MsgBeginRedelegate],
+    ["/mele.staking.v1beta1.MsgCreateValidator", MsgCreateValidator],
+    ["/mele.staking.v1beta1.MsgDelegate", MsgDelegate],
+    ["/mele.staking.v1beta1.MsgEditValidator", MsgEditValidator],
+    ["/mele.staking.v1beta1.MsgUndelegate", MsgUndelegate],
 ]
 
 function createDefaultRegistry(): Registry {
