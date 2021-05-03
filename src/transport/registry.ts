48387aa531bcbeb72e3cc42ba4aae6bd7631393f
Andrej Budinčević <andrew@hotmail.rs>
2021-05-03 22:16:49 +0200

    Register new protobuf codecs for the treasury

--- src/transport/registry.ts
+++ src/transport/registry.ts
@@ -7,13 +7,24 @@ import {
     MsgWithdrawDelegatorReward,
     MsgWithdrawValidatorCommission,
   } from "./codec/cosmos/distribution/v1beta1/tx";
-  import {
+import {
     MsgBeginRedelegate,
     MsgCreateValidator,
     MsgDelegate,
     MsgEditValidator,
     MsgUndelegate,
-  } from "./codec/cosmos/staking/v1beta1/tx";
+} from "./codec/cosmos/staking/v1beta1/tx";
+
+import {
+  MsgAddOperator,
+  MsgRemoveOperator,
+  MsgDisburse,
+  MsgCancelDisbursement,
+  MsgApproveDisbursement,
+  MsgBurn,
+  MsgCancelBurn,
+  MsgApproveBurn,
+} from "./codec/mele/treasury/v1beta1/tx";
 
 export const defaultRegistryTypes: ReadonlyArray<[string, GeneratedType]> = [
     ['/cosmos.bank.v1beta1.MsgMultiSend', MsgMultiSend],
@@ -26,6 +37,14 @@ export const defaultRegistryTypes: ReadonlyArray<[string, GeneratedType]> = [
     ["/mele.staking.v1beta1.MsgDelegate", MsgDelegate],
     ["/mele.staking.v1beta1.MsgEditValidator", MsgEditValidator],
     ["/mele.staking.v1beta1.MsgUndelegate", MsgUndelegate],
+    ["/mele.treasury.v1beta1.MsgAddOperator", MsgAddOperator],
+    ["/mele.treasury.v1beta1.MsgRemoveOperator", MsgRemoveOperator],
+    ["/mele.treasury.v1beta1.MsgDisburse", MsgDisburse],
+    ["/mele.treasury.v1beta1.MsgCancelDisbursement", MsgCancelDisbursement],
+    ["/mele.treasury.v1beta1.MsgApproveDisbursement", MsgApproveDisbursement],
+    ["/mele.treasury.v1beta1.MsgBurn", MsgBurn],
+    ["/mele.treasury.v1beta1.MsgCancelBurn", MsgCancelBurn],
+    ["/mele.treasury.v1beta1.MsgApproveBurn", MsgApproveBurn],
 ]
 
 function createDefaultRegistry(): Registry {
