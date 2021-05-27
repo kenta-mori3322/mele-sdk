ea0912e144c88316222eb0c951a75ef83d2453d6
Andrej Budinčević <andrew@hotmail.rs>
2021-05-27 14:13:41 +0200

    Fix old upgrade module example to work with the new version

--- examples/upgrade.js
+++ examples/upgrade.js
@@ -1,14 +1,11 @@
 const { Mele, MnemonicSigner, Utils } = require('../lib/mele-sdk.cjs.js')
 const chalk = require('chalk')
 
-const CHAIN_ID = 'testnet'
-const RPC = 'http://18.203.56.151:26657/'
+const CHAIN_ID = 'test'
+const RPC = 'http://localhost:26657/'
 
 const validator1Mnemonic =
-    'excuse river blur hair devote bonus fit rookie blouse auto acoustic auto announce mystery slam exotic visit admit mule emotion voice clap tongue ramp'
-const validator2Mnemonic =
-    'honey fringe auto crucial holiday control auto reduce blind nephew piano cable intact medal wife insane response year awesome short when give bargain model'
-
+    'pet apart myth reflect stuff force attract taste caught fit exact ice slide sheriff state since unusual gaze practice course mesh magnet ozone purchase'
 const testMnemonic =
     'betray theory cargo way left cricket doll room donkey wire reunion fall left surprise hamster corn village happy bulb token artist twelve whisper expire'
 
@@ -24,18 +21,12 @@ const meleValidator1 = new Mele({
     signer: new MnemonicSigner(validator1Mnemonic),
 })
 
-const meleValidator2 = new Mele({
-    nodeUrl: RPC,
-    chainId: CHAIN_ID,
-    signer: new MnemonicSigner(validator2Mnemonic),
-})
-
 ;(async () => {
     /* Step 1
        Create a text proposal
     */
     console.log(chalk.cyan('1. Create a software upgrade proposal'))
-    let txEvents = mele.governance
+    let txEvents = await mele.governance
         .submitSoftwareUpgradeProposal(
             [
                 {
@@ -75,7 +66,7 @@ const meleValidator2 = new Mele({
        Deposit funds
     */
     console.log(chalk.cyan('2. Deposit funds'))
-    txEvents = mele.governance
+    txEvents = await mele.governance
         .deposit(proposalId, [
             {
                 denom: 'umelg',
@@ -92,7 +83,7 @@ const meleValidator2 = new Mele({
        Vote from validator 1
     */
     console.log(chalk.cyan('3. Vote from validator 1'))
-    txEvents = meleValidator1.governance
+    txEvents = await meleValidator1.governance
         .vote(proposalId, 'yes')
         .sendTransaction()
 
@@ -100,17 +91,6 @@ const meleValidator2 = new Mele({
     let votes = await mele.query.governance.getVotes(proposalId)
     console.log(JSON.stringify(votes, null, 4))
 
-    /* Step 4
-       Vote from validator 2
-    */
-    console.log(chalk.cyan('4. Vote from validator 2'))
-    txEvents = meleValidator2.governance
-        .vote(proposalId, 'yes')
-        .sendTransaction()
-
-    tx = await Utils.promisify(txEvents)
-    votes = await mele.query.governance.getVotes(proposalId)
-    console.log(JSON.stringify(votes, null, 4))
 
     /* Step 5
        Wait for voting period to end
