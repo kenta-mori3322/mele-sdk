42bf929febf94583262fe1caa57922d379784a82
Andrej Budinčević <andrew@hotmail.rs>
2021-06-14 19:54:57 +0200

    Add calculate fees to the Transaction class

--- src/transactions/index.ts
+++ src/transactions/index.ts
@@ -4,15 +4,21 @@ import { TransactionEvents } from './events'
 export class Transaction {
     private _msgs: any[]
     private _sendTransaction: Function
+    private _calculateFees: Function
 
-    constructor(msgs: any[], sendTransaction: Function) {
+    constructor(msgs: any[], sendTransaction: Function, calculateFees: Function) {
         this._msgs = msgs
 
         this._sendTransaction = sendTransaction
+        this._calculateFees = calculateFees
     }
 
     sendTransaction(): TransactionEvents {
         return this._sendTransaction(this._msgs)
+    }
+
+    async calculateFees(): Promise<number> {
+        return this._calculateFees(this._msgs)
     }
 }
 
